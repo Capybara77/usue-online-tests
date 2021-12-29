e2e0879a134b17326bd97b114ec100a5dab19527
Capybara77 <andrei7318@yandex.ru>
2021-12-29 16:46:14 +0500

    Добавьте файлы проекта.

---
+++ usue online tests/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace usue_online_tests.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DataContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                "   Host=localhost;Port=5432;Database=usue_online_tests;Username=postgres;Password=padmin");
+        }
+    }
+}
