0230f108ad8a18a33d4617640ac23080e6c51485
valeriyshashkin <velianjanjan@gmail.com>
2022-02-12 14:23:07 +0500

    добавил parcel

---
+++ usue online tests/Frontend/index.js
@@ -0,0 +1,3 @@
+import "./test.js";
+
+console.log("Привет, это код из бандла, который сделал Parcel");
---
+++ usue online tests/wwwroot/bundles/index.js
@@ -0,0 +1,2 @@
+console.log("test.js"),console.log("Привет, это код из бандла, который сделал Parcel");
+//# sourceMappingURL=index.js.map
