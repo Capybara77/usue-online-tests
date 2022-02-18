0186d44203b2745295339253e16d8e740f6357c0
valeriyshashkin <velianjanjan@gmail.com>
2022-02-19 00:15:17 +0500

    стилизовал тесты

--- usue online tests/Frontend/navigation.js
+++ usue online tests/Frontend/navigation.js
@@ -3,6 +3,14 @@ const profileMenu = document.querySelector(".navigation-profile-menu");
 const mobileNavigation = document.querySelector(".navigation-mobile");
 const mobileMenu = document.querySelector(".navigation-mobile-menu");
 
+document.documentElement.addEventListener("click", (e) => {
+  if (!profileMenu.contains(e.target) && e.target !== navigationProfile) {
+    console.log(e.target);
+    console.log(navigationProfile)
+    profileMenu.classList.add("hidden");
+  }
+});
+
 navigationProfile?.addEventListener("click", () => {
   if (matchMedia("(min-width: 768px)").matches) {
     profileMenu.classList.toggle("hidden");
--- usue online tests/wwwroot/bundles/index.js
+++ usue online tests/wwwroot/bundles/index.js
@@ -528,6 +528,13 @@ const navigationProfile = document.querySelector(".navigation-profile");
 const profileMenu = document.querySelector(".navigation-profile-menu");
 const mobileNavigation = document.querySelector(".navigation-mobile");
 const mobileMenu = document.querySelector(".navigation-mobile-menu");
+document.documentElement.addEventListener("click", (e)=>{
+    if (!profileMenu.contains(e.target) && e.target !== navigationProfile) {
+        console.log(e.target);
+        console.log(navigationProfile);
+        profileMenu.classList.add("hidden");
+    }
+});
 navigationProfile?.addEventListener("click", ()=>{
     if (matchMedia("(min-width: 768px)").matches) profileMenu.classList.toggle("hidden");
 });
