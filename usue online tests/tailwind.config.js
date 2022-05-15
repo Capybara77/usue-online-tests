8930867a4cad75f73a892d3050ce4a5f8e9e48a2
valeriyshashkin <velianjanjan@gmail.com>
2022-05-15 18:13:12 +0500

    переделываю интерфейс на mobile first, так логичнее и проще

--- usue online tests/tailwind.config.js
+++ usue online tests/tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   content: ["./Views/**/*.cshtml"],
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  daisyui: {
+    themes: ["corporate", "business"],
+  },
 };
