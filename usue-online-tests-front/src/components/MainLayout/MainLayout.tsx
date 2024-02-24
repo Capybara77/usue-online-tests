f10716321ade11815e8a332e6f29f0bf2cb7fe0b
ilya <ilya.prikhodko22@gmail.com>
2024-02-24 18:13:32 +0500

    респонсив навигейшн

--- usue-online-tests-front/src/components/MainLayout/MainLayout.tsx
+++ usue-online-tests-front/src/components/MainLayout/MainLayout.tsx
@@ -1,17 +1,110 @@
 import { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 
+const NavBar = () => {
+  return (
+    <div className="navbar max-w-screen-lg mx-auto p-0">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <Link to={'/'} className="text-xl link link-hover">
+          Аудиторные тесты
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          <div className="avatar placeholder cursor-pointer">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost rounded-full bg-neutral "
+            >
+              <span className="text-xl text-neutral-content">AI</span>
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex min-h-screen flex-col bg-base-200 px-4">
-      <nav className="max-w-screen-lg mx-auto navbar p-0">
-        <div className="flex-1">
-          <Link to={'/'} className="text-xl">
-            Аудиторные тесты
-          </Link>
-        </div>
-      </nav>
-      <div className="grow max-w-screen-lg mx-auto w-full">{children}</div>
+      <NavBar />
+      <div className="grow max-w-screen-lg mx-auto w-full px-4 lg:px-0">{children}</div>
       <footer className="footer footer-center p-4 text-base-content">
         <div className="footer-center p-4 text-base-content">
           © 2024 - ФГАОУ ВО «УрФУ имени первого Президента России Б.Н. Ельцина»
