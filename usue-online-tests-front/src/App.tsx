2c509a1d904e01279a52b055369ee7fe3a1ac7c9
ilya <ilya.prikhodko22@gmail.com>
2024-02-24 17:34:25 +0500

    Suspense

--- usue-online-tests-front/src/App.tsx
+++ usue-online-tests-front/src/App.tsx
@@ -1,20 +1,51 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React from 'react';
+import React, { ReactNode, Suspense } from 'react';
 
 const LoginPage = React.lazy(() => import('./pages/LoginPage'));
 const UserPage = React.lazy(() => import('./pages/UserPage'));
 const TestsPage = React.lazy(() => import('./pages/TestsPage'));
 const TrainingTestPage = React.lazy(() => import('./pages/TrainingTestPage'));
 
+const SuspenseComponent = ({ children }: { children: ReactNode }) => {
+  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
+};
+
 function App() {
-  // TODO lazy loading
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/user" element={<UserPage />} />
-        <Route path="/alltests" element={<TestsPage />} />
-        <Route path="/test/:testid" element={<TrainingTestPage />} />
+        <Route
+          path="/"
+          element={
+            <SuspenseComponent>
+              <LoginPage />
+            </SuspenseComponent>
+          }
+        />
+        <Route
+          path="/user"
+          element={
+            <SuspenseComponent>
+              <UserPage />
+            </SuspenseComponent>
+          }
+        />
+        <Route
+          path="/alltests"
+          element={
+            <SuspenseComponent>
+              <TestsPage />
+            </SuspenseComponent>
+          }
+        />
+        <Route
+          path="/test/:testid"
+          element={
+            <SuspenseComponent>
+              <TrainingTestPage />
+            </SuspenseComponent>
+          }
+        />
       </Routes>
     </Router>
   );
