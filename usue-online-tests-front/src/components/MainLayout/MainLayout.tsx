f78cd9d29afd003fad0713659abbc71edd30f81b
ilya <ilya.prikhodko22@gmail.com>
2024-02-24 19:33:29 +0500

    fix key

--- usue-online-tests-front/src/components/MainLayout/MainLayout.tsx
+++ usue-online-tests-front/src/components/MainLayout/MainLayout.tsx
@@ -28,7 +28,7 @@ const NavBar = () => {
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             {links.map((link) => (
-              <li>
+              <li key={link.name}>
                 {link.sublinks?.length ? (
                   <>
                     <Link to={link.to}>{link.name}</Link>
@@ -47,23 +47,6 @@ const NavBar = () => {
                 )}
               </li>
             ))}
-            {/* <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li> */}
           </ul>
         </div>
         <Link to={'/'} className="text-xl link link-hover">
@@ -92,25 +75,6 @@ const NavBar = () => {
               )}
             </li>
           ))}
-          {/* <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li> */}
         </ul>
       </div>
       <div className="navbar-end">
