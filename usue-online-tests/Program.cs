4c48af4a69fe1097249aee3b14d7ba23e512fbb4
Capybara77 <andrei7318@yandex.ru>
2025-06-08 12:07:15 +0500

    автоматическая миграция

--- usue-online-tests/Program.cs
+++ usue-online-tests/Program.cs
@@ -1,28 +1,46 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using usue_online_tests.Data;
 
-namespace usue_online_tests
+namespace usue_online_tests;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        var host = CreateHostBuilder(args).Build();
+        MigrateDatabase(host);
+        host.Run();
     }
+    private static void MigrateDatabase(IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        try
+        {
+            var dbContext = services.GetRequiredService<DataContext>();
+
+            if (dbContext.Database.IsRelational())
+            {
+                dbContext.Database.Migrate();
+            }
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred while migrating the database.");
+        }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
